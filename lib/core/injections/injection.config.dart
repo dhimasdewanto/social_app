ddf25fa6431bf1b39bee4eae7fb9a36c38d0ac29
Dhimas Dewanto <dhimasbrd@gmail.com>
2020-09-13 00:42:49 +0700

    Fix error

--- lib/core/injections/injection.config.dart
+++ lib/core/injections/injection.config.dart
@@ -6,7 +6,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
-import 'package:injectable/get_it_helper.dart';
+import 'package:injectable/injectable.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import '../../features/auth/presentation/blocs/auth/auth_bloc.dart';
@@ -18,22 +18,27 @@ import '../../features/auth/domain/use_cases/sign_out.dart';
 /// adds generated dependencies
 /// to the provided [GetIt] instance
 
-void $initGetIt(GetIt g, {String environment}) {
-  final gh = GetItHelper(g, environment);
+GetIt $initGetIt(
+  GetIt get, {
+  String environment,
+  EnvironmentFilter environmentFilter,
+}) {
+  final gh = GetItHelper(get, environment, environmentFilter);
   final registerModules = _$RegisterModules();
   gh.lazySingleton<FirebaseAuth>(() => registerModules.firebaseAuth);
   gh.lazySingleton<GoogleSignIn>(() => registerModules.googleSignIn);
   gh.lazySingleton<SignInGoogle>(() => SignInGoogle(
-      firebaseAuth: g<FirebaseAuth>(), googleSignIn: g<GoogleSignIn>()));
+      firebaseAuth: get<FirebaseAuth>(), googleSignIn: get<GoogleSignIn>()));
   gh.lazySingleton<SignOut>(() => SignOut(
-      firebaseAuth: g<FirebaseAuth>(), googleSignIn: g<GoogleSignIn>()));
+      firebaseAuth: get<FirebaseAuth>(), googleSignIn: get<GoogleSignIn>()));
   gh.lazySingleton<GetSignedInUser>(() => GetSignedInUser(
-      firebaseAuth: g<FirebaseAuth>(), googleSignIn: g<GoogleSignIn>()));
+      firebaseAuth: get<FirebaseAuth>(), googleSignIn: get<GoogleSignIn>()));
   gh.factory<AuthBloc>(() => AuthBloc(
-        getSignedInUser: g<GetSignedInUser>(),
-        signInGoogle: g<SignInGoogle>(),
-        signOut: g<SignOut>(),
+        getSignedInUser: get<GetSignedInUser>(),
+        signInGoogle: get<SignInGoogle>(),
+        signOut: get<SignOut>(),
       ));
+  return get;
 }
 
 class _$RegisterModules extends RegisterModules {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/configs/app_settings.dart';
 import 'core/injections/injection.dart';
 import 'features/utils/presentation/pages/initial_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   configureDependencies();
   runApp(const MyApp());
 }
